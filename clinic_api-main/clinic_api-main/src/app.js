09cea7583416888c10f44d51bbd3a67e9c3db4d9
MAYCON GIBSON NEVES DOS SANTOS <maycon_g_santos@estudante.sesisenai.org.br>
2025-08-19 22:07:06 -0300

    endpoints

--- clinic_api-main/clinic_api-main/src/app.js
+++ clinic_api-main/clinic_api-main/src/app.js
@@ -27,13 +27,72 @@ app.get("/usuarios/:id", async(req,res) => {
         })
 
         if(usuarios == null){
-            return res.json("Usuário não existe");
+            return res.status(404).send("Usuário não existe");
         }
 
         return res.json(usuarios)
     }
     catch (e){
         console.log(e)
+    }
+})
+
+app.post("/usuarios", async(req, res)=> {
+  try {
+    const { body } = req
+    const usuario = await prismaClient.usuario.create({
+      data: {
+        nome: body.nome,
+        cargo: body.cargo,
+        email: body.email,
+        senha: body.senha
+      },
+    })
+    return res.status(201).json(usuario)
+  } catch (error) {
+    console.error(error)
+    if(error.code === "P2002"){
+      res.status(404).send("Falha ao cadastrar usuário: Email já cadastrado!")
+    }
+  }
+})
+
+app.put("/usuarios/:id", async(req,res)=>{
+    try {
+        const { body, params } = req
+
+        if(body.nome || body.cargo || body.email || body.senha){
+            await prismaClient.usuario.update({
+                where: {id: Number(params.id)},
+                data: {
+                    ...body
+                },
+            })
+            const usuarioAtualizado = await prismaClient.usuario.findUnique({
+                where: {
+                    id: Number(params.id)
+                }
+            })
+    
+            res.status(201).json({
+                message: "Usuário atualizado!",
+                data: usuarioAtualizado
+            })
+        } else {
+            res.status(404).send("Atributos inválidos não condizem com o schema")
+        }
+
+
+
+    } catch(error) {
+        if(error.code === "P2025"){
+            res.status(404).send("Nenhum usuário foi encontrado para uma atualização: Tem certeza que o ID está certo?")
+        }
+        if(error.code === "P2002"){
+            res.status(404).send("Esse email já foi registrado: Tente novamente!")
+        }
+
+        console.log(error)
     }
 })
 
